9a39c3edd7e136530b169dd5b5b7372f0ac7e3c7
danieljraine <ley.danielj@gmail.com>
2016-12-04 18:35:23 -0500

    use jsdom instead of phantom

--- test/octocat-browser-test.js
+++ test/octocat-browser-test.js
@@ -1,4 +1,6 @@
 'use strict';
+// Node CLI command: > require('jsdom').env("https://github.com/octocat", ["http://code.jquery.com/jquery.js"], function(err, window) { let avatar = window.$(".vcard-avatar .avatar").html()[0];})
+
 // CLI command:
 // format for shell command
 // $ phantomjs node_moduls/mocha-phantomjs-core/mocha-phantomjs-core.js <TESTS> <REPORTER> <CONFIG as JSON>
@@ -6,37 +8,34 @@
 // $ phantomjs node_modules/mocha-phantomjs-core/mocha-phantomjs-core.js test/octocat-browser-test.js
 
 // jshint esversion: 6, strict: global, node: true, mocha: true
-// let casper = require('casper').create();
-if (typeof initMochaPhantomJS === 'function') {
-    initMochaPhantomJS();
-}
 
 let chai = require('chai'),
     chaiHttp = require('chai-http'),
     sinon = require('sinon'),
     chaiAsPromised = require('chai-as-promised'),
+    jsdom = require('jsdom'),
+    $ = require('jquery'),
     expect = chai.expect,
-
+    assert = chai.assert,
     should = chai.should();
 
-chai.use(chaiHttp);
+// chai.use(chaiHttp);
 
 describe('GitHub User Profile', function(){
-    it('Displays a profile picture', (done) => {
+    let profileAvatar;
+    jsdom.env(
+        "https://github.com/octocat",
+        ["http://code.jquery.com/jquery.js"],
+        {url: "https://github.com/octocat"},
+        function (err, window) {
+            profileAvatar = window.$(".vcard-avatar .avatar").html()[0];
+        }
 
-        // let driver = browser.driver;
-        // driver.get("https://www.github.com/octocat");
-        // driver.findElements(by.src(''))
-        //       .then(function(array){
-        //             expect(array.length).not.toBe(0);
-        //         });
-        // this.timeout(10000);
-        // page.open('https://github.com/octocat', function(status) {
-        //     let content = page.content;
-        //     console.log("content: " + content);
-        //     // console.log('Content: ' + content);
-        //     phantom.exit();
-        // });
+    );
+
+    it('Displays a profile picture', (done) => {
+        // expect(profileAvatar).to.exist;  
+        assert.isObject(profileAvatar, 'profile avatar is an object');                              
         done();
     });
 });
